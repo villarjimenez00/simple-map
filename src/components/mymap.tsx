9d419cde7de9610e7ad6b21998c0c1e60739a3d0
Alejandro Villar Jimenez <villarjimenez00@gmail.com>
2020-03-14 23:43:54 +0100

    example working

---
+++ src/components/mymap.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Map, TileLayer } from 'react-leaflet'
+import { LatLngTuple } from 'leaflet'
+
+//const defaultLatLng: LatLngTuple = [48.865572, 2.283523]
+const defaultLatLng: LatLngTuple = [40.3914663, -3.6631075]
+//40.3914663,-3.6631075,17
+const zoom: number = 18
+
+const MyMap: React.FC = () => {
+  return (
+    <Map id="mapId" center={defaultLatLng} zoom={zoom}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      ></TileLayer>
+    </Map>
+  )
+}
+
+export default MyMap
