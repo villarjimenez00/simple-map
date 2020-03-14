1adb05a5b4956ffae9879ac4b6a1701428871dad
Alejandro Villar Jimenez <villarjimenez00@gmail.com>
2020-03-14 23:51:42 +0100

    fix error

--- src/components/mymap.tsx
+++ src/components/mymap.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { Map, TileLayer } from 'react-leaflet'
 import { LatLngTuple } from 'leaflet'
 
-//const defaultLatLng: LatLngTuple = [48.865572, 2.283523]
 const defaultLatLng: LatLngTuple = [40.3914663, -3.6631075]
-//40.3914663,-3.6631075,17
+
 const zoom: number = 18
 
 const MyMap: React.FC = () => {
